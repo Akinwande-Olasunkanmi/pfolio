0625a3212e4ffe7602656f3969010e0229b7fe3a
Akinwande Olasunkanmi <akinwandeolasunkanmi@gmail.com>
2024-03-27 05:54:48 +0100

    cv update

--- main.js
+++ main.js
@@ -89,7 +89,11 @@ sr.reveal('.about_img', {delay: 500})
 sr.reveal('.about_subtitle', {delay: 300})
 sr.reveal('.about_profession', {delay: 400})
 sr.reveal('.about_text', {delay: 500})
-sr.reveal('.about_social-icon', {delay: 600, interval: 200})
+sr.reveal('.about_social-icon', {origin: 'top', delay: 500, interval: 200})
+
+// cv
+sr.reveal('#cv', {delay: 600, interval: 200})
+
 
 // scroll skills
 sr.reveal('.skills_subtitle', {})
